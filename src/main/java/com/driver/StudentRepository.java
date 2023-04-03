c690aa409b4ade31446587f6e8a7d66b972d4cf9
unknown <architasinghcse@gmail.com>
2023-04-03 09:33:41 +0530

    classroom commit

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,85 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class StudentRepository {
+
+    HashMap<String,Student> studentDb=new HashMap<>();
+    HashMap<String,Teacher> teacherDb=new HashMap<>();
+    HashMap<String,List<String>> teacherStudent = new HashMap<>();
+
+
+    public void addStudent(Student student){
+        studentDb.put(student.getName(),student);
+    }
+    public void addTeacher(Teacher teacher){
+        teacherDb.put(teacher.getName(),teacher);
+    }
+
+    public void addStudentTeacher(String student,String teacher){
+        List<String> students = teacherStudent.get(teacher);
+
+        if(students==null){
+            students = new ArrayList<>();
+        }
+
+        students.add(student);
+
+        teacherStudent.put(teacher,students);
+
+
+        return ;
+    }
+
+    public Student getStudent(String name) {
+        return studentDb.get(name);
+    }
+
+    public Teacher getTeacher(String name) {
+        return teacherDb.get(name);
+    }
+
+    public List<String> getStudentByTeacherName(String teacher) {
+        return teacherStudent.get(teacher);
+    }
+
+    public List<String> getAllStudent() {
+        List<String> students = new ArrayList<>();
+
+        for(String s : studentDb.keySet()){
+            students.add(s);
+        }
+        return students;
+    }
+
+    public void deleteTeacherByName(String teacher) {
+        for(String str : teacherStudent.get(teacher)){
+
+            studentDb.remove(str);
+
+        }
+        teacherStudent.remove(teacher);
+        teacherDb.remove(teacher);
+    }
+
+    public void deleteAllTeacher() {
+        for(String teacher : teacherDb.keySet())
+        {
+            for(String str : teacherStudent.get(teacher)){
+
+                studentDb.remove(str);
+
+            }
+            teacherStudent.remove(teacher);
+            teacherDb.remove(teacher);
+
+        }
+    }
+}
